1e77b015b8adfd93d554d6c4e5ae5272c2ff6eba
abari <mehdi.abari@gmail.com>
2025-01-06 19:05:54 +0100

    z

---
+++ js/download.js
@@ -0,0 +1,9 @@
+﻿function downloadFile(filename, fileContent) {
+
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + fileContent;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
---
+++ js/downloadFile.js
@@ -0,0 +1,11 @@
+﻿function downloadFile(fileContent) {
+    const blob = new Blob([fileContent.content], { type: fileContent.contentType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileContent.fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
